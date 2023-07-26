60aeeced1ed0eb3fb322001fbd5059a7372d9757
Gaurav-Jethuri <105909298+EndGaurav@users.noreply.github.com>
2023-07-26 18:07:59 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! modified for checking purpose'
+    return 'Hello, world! modified for checking purpose, last modified.'
 
 if __name__ == '__main__':
     app.run()
